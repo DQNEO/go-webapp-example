828e2ef897952cf7000c6a0a4c2d84755897be2d
DQNEO <dqneoo@gmail.com>
2017-07-28 21:52:50 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+import "log"
+import "fmt"
+import "html"
+
+func main() {
+
+	//http.Handle("/foo", fooHandler)
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
