19ac2747a245b64c582a2205575e9f8a9ee2ed5d
DQNEO <dqneoo@gmail.com>
2017-08-12 16:15:37 +0900

    add more handler

--- main.go
+++ main.go
@@ -10,7 +10,15 @@ func main() {
 	//http.Handle("/foo", fooHandler)
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "path is %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hello.html", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
+
+	http.HandleFunc("/hello.json", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"msg":"hello"}`)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
