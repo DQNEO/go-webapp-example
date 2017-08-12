2022e536fb0afff768c7b6d22f5aaacad3a96636
DQNEO <dqneoo@gmail.com>
2017-08-12 16:34:35 +0900

    add handler for /issues and /issue/1

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "log"
 import "fmt"
 import "html"
 
+type Issue struct {
+	Id   int
+	Name string
+}
+
 func main() {
 
 	//http.Handle("/foo", fooHandler)
@@ -20,6 +25,20 @@ func main() {
 	http.HandleFunc("/hello.json", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{"msg":"hello"}`)
 	})
+
+	issues := []Issue{
+		Issue{Id:1, Name:"I need a help"},
+		Issue{Id:2, Name:"I need another help"},
+	}
+
+	http.HandleFunc("/issues", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `%v`, issues)
+	})
+
+	http.HandleFunc("/issues/1", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w,`%v`, issues[0])
+	})
+
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
