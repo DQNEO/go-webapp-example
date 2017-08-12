c506c2689235e86b62d5c91a42bf33f72e5576b7
DQNEO <dqneoo@gmail.com>
2017-08-13 00:22:16 +0900

    inline

--- route/route.go
+++ route/route.go
@@ -4,7 +4,7 @@ import "net/http"
 import "../handler"
 
 func Get(path string, h func(http.ResponseWriter, *http.Request)) {
-	http.DefaultServeMux.HandleFunc(path, h)
+	http.DefaultServeMux.Handle(path, http.HandlerFunc(h))
 }
 
 func Register() {
