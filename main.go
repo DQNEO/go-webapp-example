f6ed1484887db65abe82ed8bc882fd158eb88298
DQNEO <dqneoo@gmail.com>
2017-08-12 16:50:11 +0900

    reformat

--- main.go
+++ main.go
@@ -8,13 +8,9 @@ func main() {
 	//http.Handle("/foo", fooHandler)
 
 	http.HandleFunc("/hello", handler.GetHello)
-
 	http.HandleFunc("/hello.html", handler.GetHelloHTML)
-
 	http.HandleFunc("/hello.json", handler.GetHelloJson)
-
 	http.HandleFunc("/issues", handler.GetIssues)
-
 	http.HandleFunc("/issues/1", handler.GetIssue1)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
