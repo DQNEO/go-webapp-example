448997608bca4458e68d8d5aef9afb0b851ff6ef
DQNEO <dqneoo@gmail.com>
2017-08-12 17:18:55 +0900

    extract Register func

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ import "./handler"
 import "net/http"
 import "log"
 
-func main() {
-
-	//http.Handle("/foo", fooHandler)
-
+func Register() {
 	http.HandleFunc("/hello", handler.GetHello)
 	http.HandleFunc("/hello.html", handler.GetHelloHTML)
 	http.HandleFunc("/hello.json", handler.GetHelloJson)
 	http.HandleFunc("/issues", handler.GetIssues)
 	http.HandleFunc("/issues/1", handler.GetIssue1)
+}
+
+func main() {
+
+	Register()
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
