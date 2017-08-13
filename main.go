2e9cef1bcd810d27ace1e75f8ec11c2915a06a48
DQNEO <dqneoo@gmail.com>
2017-08-13 22:56:08 +0900

    gofmt -w .

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 )
 
 type Router struct {
-	SimpleMaps map[string] map[string] http.HandlerFunc
-	RegexMaps  map[string] map[*regexp.Regexp] http.HandlerFunc
+	SimpleMaps map[string]map[string]http.HandlerFunc
+	RegexMaps  map[string]map[*regexp.Regexp]http.HandlerFunc
 }
 
-func(rt *Router) RegisterHandler(method string ,pattern string, h func(http.ResponseWriter, *http.Request)) {
+func (rt *Router) RegisterHandler(method string, pattern string, h func(http.ResponseWriter, *http.Request)) {
 	if strings.Contains(pattern, "{id}") {
-		newPattern := strings.Replace(pattern,"{id}", "([a-zA-Z0-9]+)", -1)
+		newPattern := strings.Replace(pattern, "{id}", "([a-zA-Z0-9]+)", -1)
 		reg := regexp.MustCompile(newPattern)
 		rt.RegexMaps[method][reg] = http.HandlerFunc(h)
 	} else {
@@ -23,22 +23,22 @@ func(rt *Router) RegisterHandler(method string ,pattern string, h func(http.Resp
 	}
 }
 
-func(rt *Router) Get(pattern string, h func(http.ResponseWriter, *http.Request)) {
+func (rt *Router) Get(pattern string, h func(http.ResponseWriter, *http.Request)) {
 	rt.RegisterHandler("GET", pattern, h)
 }
 
-func(rt *Router) Post(pattern string, h func(http.ResponseWriter, *http.Request)) {
+func (rt *Router) Post(pattern string, h func(http.ResponseWriter, *http.Request)) {
 	rt.RegisterHandler("POST", pattern, h)
 }
 
-func(rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// test complete match
-	if k,ok := rt.SimpleMaps[r.Method][r.URL.Path]; ok {
+	if k, ok := rt.SimpleMaps[r.Method][r.URL.Path]; ok {
 		k(w, r)
 		return
 	}
 
-	for reg,h := range(rt.RegexMaps[r.Method]) {
+	for reg, h := range rt.RegexMaps[r.Method] {
 		if matches := reg.FindAllStringSubmatch(r.URL.Path, -1); matches != nil {
 			handler.URLParam = matches
 			h(w, r)
@@ -54,12 +54,12 @@ func(rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func Register() *Router {
 	rt := &Router{}
 
-	rt.SimpleMaps = make(map[string] map[string] http.HandlerFunc)
-	rt.RegexMaps = make(map[string] map[*regexp.Regexp] http.HandlerFunc)
+	rt.SimpleMaps = make(map[string]map[string]http.HandlerFunc)
+	rt.RegexMaps = make(map[string]map[*regexp.Regexp]http.HandlerFunc)
 	rt.SimpleMaps["GET"] = make(map[string]http.HandlerFunc)
-	rt.RegexMaps["GET"] = make(map[*regexp.Regexp] http.HandlerFunc)
+	rt.RegexMaps["GET"] = make(map[*regexp.Regexp]http.HandlerFunc)
 	rt.SimpleMaps["POST"] = make(map[string]http.HandlerFunc)
-	rt.RegexMaps["POST"] = make(map[*regexp.Regexp] http.HandlerFunc)
+	rt.RegexMaps["POST"] = make(map[*regexp.Regexp]http.HandlerFunc)
 
 	rt.Get("/hello", handler.GetHello)
 	rt.Get("/hello.html", handler.GetHelloHTML)
