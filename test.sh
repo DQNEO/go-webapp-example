c7376cee97d1718f5a898c2e34e274b7213a68a4
DQNEO <dqneoo@gmail.com>
2017-08-14 10:33:43 +0900

    test post

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ set -x
 BASE_URL=http://localhost:9000
 mkdir -p .test
 
+# test GET
 curl --silent $BASE_URL/hello -o ./.test/hello
 test $(jq -r '.msg' < .test/hello) = hello
 
@@ -12,7 +13,9 @@ curl --silent $BASE_URL/issues -o ./.test/issues
 test $(jq -r '. | length' < .test/issues) = 2
 test "$(jq -r '.[1].Name' < .test/issues)" = "I need another help"
 
-
+# test POST
+curl --silent -X POST $BASE_URL/hello -o ./.test/post_hello
+test "$(jq -r '.msg' < .test/post_hello)" = "post hello"
 
 echo "ok"
 
